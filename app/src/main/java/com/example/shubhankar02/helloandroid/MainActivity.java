f84e13c51b983566f4d34bcff56e7a7e7a9d0772
Shubhankar02 <shborade@gmail.com>
2018-03-22 12:45:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shubhankar02/helloandroid/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.shubhankar02.helloandroid;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+import static android.content.Intent.ACTION_VIEW;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
